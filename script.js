ddec59ee530a982c9c0eb41fd30de969ef746dd8
Alicia Zavala <alicia@ku.edu>
2020-02-09 02:25:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
-function dropDown() {
-  document.getElementById("UserMenu").classList.toggle("show");
+function country(x) {
+  switch(x.value) {
+    case "Blv":
+      document.getElementById("bolivia").classList.toggle("show")
+      
+  }
 }
 
-// Close the dropdown menu if the user clicks outside of it
+function AMOUNT(){
+  var input = document.getElementById("input").value;
+  document.getElementById("result").innerText = input * 0.52;
+}
+
+function dropDown() {
+  document.getElementById("UserMenu").classList.toggle("show");
+  document.getElementById("CountryMenu").classList.toggle("show");
+}
+function dropDown2() {
+  document.getElementById("CountryMenu").classList.toggle("show");
+}
+
 window.onclick = function(event) {
   if (!event.target.matches('.username')) {
     var dropdowns = document.getElementsByClassName("user-menu");
@@ -15,4 +31,5 @@ window.onclick = function(event) {
       }
     }
   }
+  
 }
